e969c4e2777455abaa4a1c673361700c279a18de
Dana Strong <dstrong@strongwares.com@>
2020-10-24 21:32:31 -0500

    with prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-// import logo from "./logo.svg";
-import logo from "./assets/icons/logo.png";
-import "./aatapp.css";
+import './aatapp.css';
 
 function App() {
   return (
     <div className="aatapp-container">
       <header className="aatapp-header">
-        <img src={logo} className="aatapp-logo" alt="logo" />
         <p>Action Account Totals</p>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeReact from 'primereact/utils';
+import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import './index.css';
+
+PrimeReact.ripple = true;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
